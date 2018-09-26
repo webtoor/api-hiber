e5069c946c99f91cc121ec3953587829911b39fb
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-09-26 18:43:07 +0700

    carbon date subject

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+
 use App\Order;
 class OrderController extends Controller
 {
     public function create(Request $request){
+        $carbon = Carbon::now();
+        return $carbon->format('d-M-Y H:i A');
+
         $this->validate($request, [
          'mulai' => 'required|string',
          'akhir' => 'required|string',
@@ -15,7 +20,9 @@ class OrderController extends Controller
          'latlng' => 'required|string',
         ]);
 
-        
+        $result_order = Order::create([
+            'subject' => $subject
+        ]);
 
     }
 }
