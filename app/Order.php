66f92a8326ca42c506c6994726de2b287c06489c
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-09-26 14:17:44 +0700

    create Ordercontroller

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $table = 'orders';
+    protected $fillable = [
+        'subject', 'createdby', 'dtprojectstart', 'dtprojectend','projecttype', 'orderhourduration', 'comment'
+    ];
+    public $timestamps = false;
 }
