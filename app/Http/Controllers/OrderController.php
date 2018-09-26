e19f2a088cb756b1dc40ab3a38bdf8eaf2500079
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-09-26 14:47:36 +0700

    create validate order create

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     public function create(Request $request){
-        
+        $this->validate($request, [
+         'dtprojectstart' => 'require|string|min:8',
+         'dtprojectend' => 'require|string|min:8',
+         'projecttype' => 'require|string',
+         'comment' => 'required|string'
+        ]);
 
     }
 }
