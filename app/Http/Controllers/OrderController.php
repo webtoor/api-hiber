1a255a4f9c2058f4a43e5d1b401936da048602f9
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-09-27 19:12:47 +0700

    make model order_output

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,20 +11,24 @@ class OrderController extends Controller
     public function create(Request $request){
         
         //return response()->json($request);
-
+        $hasil_array = $request->json('hasil');
+      
         $this->validate($request, [
          'mulai' => 'required|string',
          'akhir' => 'required|string',
          'kegunaan' => 'required|string',
         ]);
-
+        
+        // Generated subject
         $carbon = Carbon::now();
         $subject =  $carbon->format('d-M-Y H:i A');
       // date("j M, Y", strtotime($b));
+        // Generated orderhours
         $mulai = Carbon::createFromFormat('Y-m-d', $request->json('mulai'));
         $akhir = Carbon::createFromFormat('Y-m-d', $request->json('akhir'));
         $orderhourduration = $mulai->diffInHours($akhir);
-         $result_order = Order::create([
+        // Store order
+           $result_order = Order::create([
             'subject' => $subject,
             'createdby' => $request->json('createdby_id'),
             'dtprojectstart' => $mulai,
@@ -33,11 +37,14 @@ class OrderController extends Controller
             'orderhourduration' => $orderhourduration,
             'comment' => 'adssda'
         ]);  
+          foreach($hasil_array as $hasil ){
+             $result_order_output = adasdsa;
+        }
         if($result_order){
             return response()->json([
                 'success' => true
                 ]);
-        }
+        } 
 
     }
 }
