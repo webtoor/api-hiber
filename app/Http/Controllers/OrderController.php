9acdd2fa9b15919a3e2bb6f7dce883dad45b2557
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-09-27 17:04:54 +0700

    get hour in two dates

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,20 +9,33 @@ use App\Order;
 class OrderController extends Controller
 {
     public function create(Request $request){
-        $carbon = Carbon::now();
-        return $carbon->format('d-M-Y H:i A');
+  
 
         $this->validate($request, [
          'mulai' => 'required|string',
          'akhir' => 'required|string',
-         'projecttype' => 'required|string',
+         'kegunaan' => 'required|string',
          'comment' => 'required|string',
          'latlng' => 'required|string',
         ]);
 
-        $result_order = Order::create([
-            'subject' => $subject
-        ]);
+        $carbon = Carbon::now();
+        $subject =  $carbon->format('d-M-Y H:i A');
+        $awal = '2018-10-15';
+        $b = '2018-10-17';
+        return response()->json(["date" => date("j M, Y", strtotime($b))]);
+        $mulai =  Carbon::createFromFormat('Y-m-d', $awal);
+        $akhir = Carbon::createFromFormat('Y-m-d', $b);
+         $orderhourduration = $mulai->diffInHours($akhir);
+      /*   $result_order = Order::create([
+            'subject' => $subject,
+            'createdby' => $request->json('createdby_id'),
+            'dtprojectstart' => $mulai,
+            'dtprojectend' => $akhir,
+            'projecttype' => $request->json('projecttype_id'),
+            'orderhourduration' => ,
+            'comment' => ''
+        ]); */
 
     }
 }
