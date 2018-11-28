ae89261c763ca28dcf7fa850eeb84c34ab0e160a
webtoor <fityanalimunshi@gmail.com>
2018-11-28 12:57:14 +0700

    method order_location + order_output

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\User;
 use App\User_role;
 use App\Order;
+use App\Order_location;
+use App\Order_output;
 
 class AdminController extends Controller
 {
@@ -38,7 +40,20 @@ class AdminController extends Controller
             } 
     }
 
-    public function orderDetailShow(){
-      return "dasdasd";
+    public function orderDetailShow($id_order){
+        $results_location = Order_location::where('order_id', $id_order)->get();
+        $results_output = Order_output::where('order_id', $id_order)->get();
+
+        if($results_location && $results_output){
+            return response()->json([
+                'success' => true,
+                'location' => $results_location,
+                'output' => $results_output
+            ]);
+            }else{
+            return response()->json([
+                'success' => false,
+            ]);
+            } 
      }
 }
