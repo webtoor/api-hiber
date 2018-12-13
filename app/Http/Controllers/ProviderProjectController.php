4c8ca45c4b6820aa932cd12e48516cdbb829f113
webtoor <fityanalimunshi@gmail.com>
2018-12-13 12:39:17 +0700

    v1.0.0#provider#2

--- app/Http/Controllers/ProviderProjectController.php
+++ app/Http/Controllers/ProviderProjectController.php
@@ -34,7 +34,12 @@ class ProviderProjectController extends Controller
 
     public function tawaranShow($provider_id){
         $status_id = '1';
-        $results = Order_status::with(['order','user_client'])->where('status_id', $status_id)->orderBy('id', 'desc')->get();  
+         $order_id = Order_proposal::where('proposal_by', $provider_id)->get();
+        foreach($order_id as $orders){
+            $array_order_id[] = $orders['order_id'];
+        }
+        //return $array_order_id;
+        $results = Order_status::with(['order','user_clients'])->where('status_id',$status_id)->whereNotIn('order_id', $array_order_id )->orderBy('id', 'desc')->get();  
 
         if($results){
             return response()->json([
