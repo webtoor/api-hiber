4072740e4abc6e2fcbdb3b1a5fd17a7712c1ae91
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2020-06-22 14:52:02 +0700

    Create Middeware Clint

---
+++ v2/app/Http/Controllers/V2/ClientController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\V2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use App\Models\Order;
+use App\Models\OrderStatus;
+use App\Models\OrderLocation;
+use App\Models\OrderOutput;
+use App\Models\OrderProposal;
+use App\Models\OrderFeedback;
+use App\Models\UserFeedback;
+use App\Models\User;
+use App\Models\DeviceToken;
+
+class ClientController extends Controller
+{
+    //
+}
